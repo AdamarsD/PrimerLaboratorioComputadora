f7fa692ceee378e95755f10dded5f6a14b4e0a6f
AdamarsD <ad0094032021@unab.edu.sv>
2021-08-23 17:50:27 -0600

    Partes de una Computadoras

---
+++ Computador/Computador/Entidades/ClsComputador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computador.Entidades
+{
+    class ClsComputador
+    {
+        // Partes internas de una computadora 
+
+        private String 
+    }
+}
